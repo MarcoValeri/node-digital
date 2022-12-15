6dee50affc0417acb3a8ca9fbbbac5d2a089371e
MarcoValeri <marcovaleri@hotmail.it>
2022-12-15 11:32:39 +0000

    Add homepage

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 // Controllers
 const adminController = require('../controllers/adminController');
+const homeControllers = require('../controllers/homeControllers');
 
 router.use('/admin/dashboard', adminController.adminDashboard);
+router.use('/', homeControllers.home);
 
 module.exports = router;
