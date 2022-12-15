8e72f9f56cb1e64bc993cfd2a468c10cacccdd1f
MarcoValeri <marcovaleri@hotmail.it>
2022-12-15 14:51:44 +0000

    Create article model

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,5 +1,14 @@
+// Models
+const Article = require('../models/Article');
+
 exports.article = (req, res, next) => {
     const url = req.params.url;
+
+    // Create Article Obj
+    const getArticle = new Article();
+    const articleObj = getArticle.showArticle(url);
+    console.log(`articleObj: ${articleObj}`);
+    console.log(JSON.stringify(articleObj));
 
     res.render('./articles/article', {
         pageTitle: url
---
+++ models/Article.js
@@ -0,0 +1,49 @@
+const mysql = require('mysql');
+
+module.exports = class Article {
+
+    addArticle(title, description, url, content) {
+        const sqlQuery = `INSERT INTO articles(title, description, url, content) VALUES('${title}', '${description}', '${url}', '${content}')`;
+
+        const connectionDb = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'node_digital'
+        });
+
+        connectionDb.connect(err => {
+            if (err) throw err;
+            console.log(`Connected to the db...`);
+
+            connectionDb.query(sqlQuery, (err, result) => {
+                if (err) throw err;
+                console.log(`New article has been added`);
+            })
+        })
+    }
+
+    async showArticle(url) {
+        const sqlQuery = `SELECT * FROM articles WHERE url='${url}'`;
+
+        const connectionDb = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'node_digital'
+        });
+
+        await connectionDb.connect(err => {
+            if (err) throw err;
+            console.log(`Connected to the db...`);
+
+            connectionDb.query(sqlQuery, (err, result) => {
+                if (err) throw err;
+                // console.log(JSON.stringify(result));
+                return result;
+            })
+        })
+
+    }
+
+}
